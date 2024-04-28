ce985de120dce167c05dbcc54ee6a3ec100abf54
adampweb <adamweb92@gmail.com>
2024-04-28 16:46:10 +0200

    useRouter test

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -1,4 +1,11 @@
+'use client';
+
+import { useRouter } from 'next/router';
+
 export default function Page({ params }: { params: { slug: string } }) {
+  const router = useRouter();
+  const slug = (router.query.slug as string[]) || [];
+
   return(
     <>
       {params.slug ? <h1>Slug: {params.slug}</h1> : <h1>Home</h1>} 
