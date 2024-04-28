f6a61a9407a600eb315f87c7230b82fe0cf4f98e
adampweb <adamweb92@gmail.com>
2024-04-28 17:04:21 +0200

    Print segments

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -1,9 +1,27 @@
 'use client';
 
-import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation';
+
+const Segment = (name: string) => {
+  return(
+    <p>{name}</p>
+  )
+}
 
 export default function Page() {
   const pathname = usePathname();
+  const segments = pathname.split("/");
+  const json_uri = JSON.stringify(segments);
 
-  return <h1>Current pathname: {pathname}</h1>
+  return (
+    <>
+      <h1>Current pathname: {pathname}</h1>
+      <code>
+        <pre>
+          {segments}
+        </pre>
+      </code>
+    </>
+  )
+
 }
