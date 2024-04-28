7c2eb3cac98497878b01caa8069647f0aee706d5
adampweb <adamweb92@gmail.com>
2024-04-28 16:47:23 +0200

    fix

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -2,13 +2,13 @@
 
 import { useRouter } from 'next/router';
 
-export default function Page({ params }: { params: { slug: string } }) {
+export default function Page() {
   const router = useRouter();
   const slug = (router.query.slug as string[]) || [];
 
   return(
     <>
-      {params.slug ? <h1>Slug: {params.slug}</h1> : <h1>Home</h1>} 
+      {slug ? <h1>Slug: {slug.join("/")}</h1> : <h1>Home</h1>} 
     </>
   );
 }
